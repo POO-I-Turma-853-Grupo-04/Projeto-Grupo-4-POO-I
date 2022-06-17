f624d5b5f7d9937bb9d6ea9835dd92c5d540fdf6
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-17 14:55:39 -0300

    Adicionando classe Veiculo

---
+++ Projeto - Grupo 04 Turma 853/Grupo_04_Turma_853/Veiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_04_Turma_853
+{
+    public class Veiculo
+    {
+        public string? Placa { get; set; }
+        public string? Modelo { get; set; }
+
+        public void ImprimirDados()
+        {
+
+        }
+    }
+}
