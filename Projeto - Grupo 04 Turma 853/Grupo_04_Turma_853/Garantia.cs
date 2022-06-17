4e7290807ab6245c5e8b2d67bc06e710e67f8380
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-16 21:43:58 -0300

    Adicionando a classe Garantia

---
+++ Projeto - Grupo 04 Turma 853/Grupo_04_Turma_853/Garantia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_04_Turma_853
+{
+    public class Garantia
+    {
+        public string? Tipo { get; private set; }
+        public decimal Valor { get; private set; }
+    }
+}
