dded0fb8543654ff2a0a7e00e99f23e633593602
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-16 21:23:48 -0300

    Commit inicial - adicionando class Main

---
+++ Projeto - Grupo 04 Turma 853/Grupo_04_Turma_853/Emprestimo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_04_Turma_853
+{
+    public class Emprestimo
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
