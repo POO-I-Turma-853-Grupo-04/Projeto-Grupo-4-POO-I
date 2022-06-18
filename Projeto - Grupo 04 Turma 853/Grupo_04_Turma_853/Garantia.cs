20e29f39a983effd74c5db5b178a48cb4f0a9ef3
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-18 13:01:15 -0300

    Adicionando a classe Garantia

--- Projeto - Grupo 04 Turma 853/Grupo_04_Turma_853/Garantia.cs
+++ Projeto - Grupo 04 Turma 853/Grupo_04_Turma_853/Garantia.cs
@@ -8,7 +8,43 @@ namespace Grupo_04_Turma_853
 {
     public class Garantia
     {
-        public string? Tipo { get; private set; }
+        // Os valores de Tipo podem vir direto da classe sistema, ex.: DATABASE_TIPO = {"Veículo", "Imóvel"}
+        // Assim, na main, podemos colocar opções para o usuário escolher
+        // Não precisaríamos fazer uma validação nesse caso :) (Só a validação da escolha)
+        // Depois teríamos um if.. que escolheria quais informações pediríamos: Imovel ou Veiculo
+        public string[] Tipo { get; private set; } = { "Veículo", "Imóvel" };
         public decimal Valor { get; private set; }
+
+        public void ImprimeListaTipos()
+        {
+            for (int i = 0; i < Tipo.Length; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {Tipo[i]}");
+            }
+        }
+
+        public bool ValidaTipo(string codigoDigitado)
+        {
+            int codigoGarantia;
+
+            if(!int.TryParse(codigoDigitado, out codigoGarantia) || codigoGarantia < 1 || codigoGarantia > Tipo.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidaValor(string valorDigitado)
+        {
+            decimal valorGarantia;
+
+            if (!decimal.TryParse(valorDigitado, out valorGarantia) || valorGarantia < 0)
+            {
+                return false;
+            }
+
+            this.Valor = valorGarantia;
+            return true;
+        }
     }
 }
