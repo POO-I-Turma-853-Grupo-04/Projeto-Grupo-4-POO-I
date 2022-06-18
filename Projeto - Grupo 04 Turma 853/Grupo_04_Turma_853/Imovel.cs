e0f893ff9f2d86a5a3dd75246d0e64f4482e6cc5
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-18 17:58:40 -0300

    Atualizando a classe Imovel

---
+++ Projeto - Grupo 04 Turma 853/Grupo_04_Turma_853/Imovel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_04_Turma_853
+{
+    public class Imovel : Garantia
+    {
+        public string? endereco;
+        public string? cep;
+
+        public bool ValidaCEP(string cep)
+        {
+            if (cep.Length != 8 || !long.TryParse(cep, out _))
+                return false;
+
+            this.cep = cep;
+            return true;
+        }
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Dados do Endereço:");
+            Console.WriteLine($"{endereco}, CEP: {this.cep?.Substring(0,5)}-{this.cep?.Substring(5,3)}");
+        }    
+    }
+}
